706eb57fc75c93674fae3e564b7cb51e90a1c73b
Fernando <fferreira12@gmail.com>
2017-03-05 11:24:37 -0300

    updated ComparerTests

--- ReaderTesting/ComparerTests.cs
+++ ReaderTesting/ComparerTests.cs
@@ -9,11 +9,24 @@ namespace ReaderTesting
     [TestClass]
     public class ComparerTests
     {
+
+        List<string> allLines;
+        Dictionary<string, Stock> allStocks;
+        Stock petr;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            allLines = Reader.GettAllLinesFromText(ReaderTesting.Properties.Resources.COTAHIST_A2016);
+            allStocks = Reader.GetAllStockData(allLines);
+            petr = allStocks["PETR3"];
+        }
+
         [TestMethod]
         public void TestRankOfBestStocks() //static
         {
 
-            Dictionary<string, Stock> allStocks = Reader.GetAllStockData("COTAHIST_A2016.TXT");
+            //Dictionary<string, Stock> allStocks = Reader.GetAllStockData("COTAHIST_A2016.TXT");
 
             MarketHistoryAnalyzer.FillAllWithDefaults(allStocks);
 
@@ -25,7 +38,7 @@ namespace ReaderTesting
         public void TestRankObjectLevel()
         {
 
-            Dictionary<string, Stock> allStocks = Reader.GetAllStockData("COTAHIST_A2016.TXT");
+            //Dictionary<string, Stock> allStocks = Reader.GetAllStockData("COTAHIST_A2016.TXT");
 
             MarketHistoryAnalyzer.FillAllWithDefaults(allStocks);
 
